62469540ba0ba1568e9d940411d76dc180fb2278
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2023-09-22 18:53:30 -0300

    - Turn table's header and rows into html5 templates
    - Split fillTitulosTable
    - Probe templates and cell's value filling

---
+++ Rucula.FrontEnd/main.js
@@ -0,0 +1,10 @@
+﻿import fillTitulosTable from "./modules/titulos-table.js";
+
+addEventListener("load", async () => {
+    await Blazor.start();
+
+    await DotNet.invokeMethodAsync('Rucula.WebAssembly', 'GetTitulosRanking');
+    const tabla = document.getElementById("tabla-titulos");
+
+    fillTitulosTable(tabla, titulos);
+});
---
+++ Rucula.FrontEnd/modules/titulos-table.js
@@ -0,0 +1,26 @@
+﻿export default function fillTitulosTable(table, titulos) {
+    CreateTableHeader(table, titulos);
+    FillTitulosRows(table, titulos);
+}
+
+function FillTitulosRows(table, titulos) {
+    const tbody = table.tBodies[0];
+    const row = document.getElementById("titulos-table-row-template").content;
+
+    for (let i = 0; i < titulos.length; i++) {
+        const clonedRow = row.cloneNode(true);
+        const cell = clonedRow.getElementById("symbol-pesos");
+        cell.innerHTML = "lalala";
+        tbody.appendChild(clonedRow);
+    }
+}
+
+function CreateTableHeader(table, titulos) {
+    const thead = table.tHead;
+    const header = document.getElementById("titulos-table-header-template").content;
+
+    for (let i = 0; i < titulos.length; i++) {
+        thead.appendChild(header.cloneNode(true));
+    }
+}
+
