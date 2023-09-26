6eb68ae89ab392c05593f825acce6f1107f556cb
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2023-09-26 17:11:32 -0300

    Fill all the table with titulos

--- Rucula.FrontEnd/main.js
+++ Rucula.FrontEnd/main.js
@@ -3,8 +3,7 @@
 addEventListener("load", async () => {
     await Blazor.start();
 
-    let titulos = [1, 2, 3, 4];
-    //titulos = await DotNet.invokeMethodAsync('Rucula.WebAssembly', 'GetTitulosRanking');
+    const titulos = await DotNet.invokeMethodAsync('Rucula.WebAssembly', 'GetTitulosRanking');
     const tabla = document.getElementById("tabla-titulos");
 
     fillTitulosTable(tabla, titulos);
--- Rucula.FrontEnd/modules/titulos-table.js
+++ Rucula.FrontEnd/modules/titulos-table.js
@@ -8,11 +8,33 @@ function FillTitulosRows(table, titulos) {
     const row = document.getElementById("titulos-table-row-template").content;
 
     for (let i = 0; i < titulos.length; i++) {
-        const clonedRow = row.cloneNode(true);
-        const cell = clonedRow.getElementById("symbol-pesos");
-        cell.innerHTML = "lalala";
-        tbody.appendChild(clonedRow);
+        AddNewRow(tbody, row, titulos[i]);
     }
+}
+
+function AddNewRow(tbody, templateRow, titulo) {
+    const clonedRow = templateRow.cloneNode(true);
+
+    WriteCell(clonedRow, "symbol-pesos", titulo.tituloPeso.simbolo);
+    WriteCell(clonedRow, "cotizacion-pesos", titulo.tituloPeso.precioVenta);
+    WriteCell(clonedRow, "symbol-cable", titulo.tituloCable.simbolo);
+    WriteCell(clonedRow, "cotizacion-cable", titulo.tituloCable.precioCompra);
+    WriteCell(clonedRow, "symbol-mep", titulo.tituloMep.simbolo);
+    WriteCell(clonedRow, "cotizacion-mep", titulo.tituloMep.precioVenta);
+    WriteCell(clonedRow, "blue-compra", titulo.blue.precioCompra);
+    WriteCell(clonedRow, "blue-venta", titulo.blue.precioVenta);
+    WriteCell(clonedRow, "ccl", titulo.cotizacionCcl);
+    WriteCell(clonedRow, "ccl-mep-blue", titulo.cotizacionCclMepBlue);
+    WriteCell(clonedRow, "porc-ccl-mep-blue", titulo.porcentajeArbitrajeCclMepBlue);
+    WriteCell(clonedRow, "porc-ccl-mep", titulo.porcentajeArbitrajeCclMep);
+    WriteCell(clonedRow, "porc-mep-blue", titulo.porcentajeRuloMepBlue);
+
+    tbody.appendChild(clonedRow);
+}
+
+function WriteCell(row, cellId, data) {
+    const cell = row.getElementById(cellId);
+    cell.innerHTML = data;
 }
 
 function CreateTableHeader(table, titulos) {
