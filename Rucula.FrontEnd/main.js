0d500271dcdf3869ca5172c6d985aabdfce54706
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2023-10-02 14:59:29 -0300

    - Added 1 minute data-refreshing timer
    - Remove all table rows when refresh data
    - Camel case fix

--- Rucula.FrontEnd/main.js
+++ Rucula.FrontEnd/main.js
@@ -1,11 +1,42 @@
 ﻿import showTitulosPublicos from "./modules/titulos-publicos.js";
 import showDolarBlue from "./modules/dolar-blue.js";
 
+
+var dataIntervalId = null;
+
 addEventListener("load", async () => {
     await Blazor.start();
 
-    showDolarBlue();
-    showTitulosPublicos();
+    getAllData();
+    startTimerGettingData();
+    hookVisibilityEvent();
 });
 
+
+function hookVisibilityEvent() {
+    document.addEventListener("visibilitychange", (e) => {
+
+        if (document.visibilityState === "visible")
+            startTimerGettingData();
+        else
+            stopTimerGettingData();
+    });
+}
+
+function startTimerGettingData() {
+    stopTimerGettingData();
+
+    const OneMinute = 60000;
+    dataIntervalId = setInterval(getAllData, OneMinute);
+}
+
+function stopTimerGettingData() {
+    if (dataIntervalId)
+        clearInterval(dataIntervalId);
+}
+
+function getAllData() {
+    showDolarBlue();
+    showTitulosPublicos();
+}
 
--- Rucula.FrontEnd/modules/titulos-publicos.js
+++ Rucula.FrontEnd/modules/titulos-publicos.js
@@ -18,51 +18,59 @@ function hideLoadingIndicator() {
 function fillTitulosTable(titulos) {
     const table = document.getElementById("tabla-titulos");
 
-    CreateTableHeader(table, titulos);
-    FillTitulosRows(table, titulos);
+    clearTableRows(table);
+    createTableHeader(table, titulos);
+    fillTitulosRows(table, titulos);
 }
 
-function FillTitulosRows(table, titulos) {
+function fillTitulosRows(table, titulos) {
     const tbody = table.tBodies[0];
     const row = document.getElementById("titulos-table-row-template").content;
 
     for (let i = 0; i < titulos.length; i++) {
-        AddNewRow(tbody, row, titulos[i]);
+        addNewRow(tbody, row, titulos[i]);
     }
 }
 
-function AddNewRow(tbody, templateRow, titulo) {
+function addNewRow(tbody, templateRow, titulo) {
     const clonedRow = templateRow.cloneNode(true);
     const numFormater = new Intl.NumberFormat('es-AR', {
         minimumFractionDigits: 3,
         maximumFractionDigits: 3,
     });
 
-    WriteCell(clonedRow, "symbol-pesos", titulo.tituloPeso.simbolo);
-    WriteCell(clonedRow, "cotizacion-pesos", numFormater.format(titulo.tituloPeso.precioVenta));
-    WriteCell(clonedRow, "symbol-cable", titulo.tituloCable.simbolo);
-    WriteCell(clonedRow, "cotizacion-cable", numFormater.format(titulo.tituloCable.precioCompra));
-    WriteCell(clonedRow, "symbol-mep", titulo.tituloMep.simbolo);
-    WriteCell(clonedRow, "cotizacion-mep", numFormater.format(titulo.tituloMep.precioVenta));
-    WriteCell(clonedRow, "ccl", numFormater.format(titulo.cotizacionCcl));
-    WriteCell(clonedRow, "ccl-mep-blue", numFormater.format(titulo.cotizacionCclMepBlue));
-    WriteCell(clonedRow, "porc-ccl-mep-blue", numFormater.format(titulo.porcentajeArbitrajeCclMepBlue));
-    WriteCell(clonedRow, "porc-ccl-mep", numFormater.format(titulo.porcentajeArbitrajeCclMep));
+    writeCell(clonedRow, "symbol-pesos", titulo.tituloPeso.simbolo);
+    writeCell(clonedRow, "cotizacion-pesos", numFormater.format(titulo.tituloPeso.precioVenta));
+    writeCell(clonedRow, "symbol-cable", titulo.tituloCable.simbolo);
+    writeCell(clonedRow, "cotizacion-cable", numFormater.format(titulo.tituloCable.precioCompra));
+    writeCell(clonedRow, "symbol-mep", titulo.tituloMep.simbolo);
+    writeCell(clonedRow, "cotizacion-mep", numFormater.format(titulo.tituloMep.precioVenta));
+    writeCell(clonedRow, "ccl", numFormater.format(titulo.cotizacionCcl));
+    writeCell(clonedRow, "ccl-mep-blue", numFormater.format(titulo.cotizacionCclMepBlue));
+    writeCell(clonedRow, "porc-ccl-mep-blue", numFormater.format(titulo.porcentajeArbitrajeCclMepBlue));
+    writeCell(clonedRow, "porc-ccl-mep", numFormater.format(titulo.porcentajeArbitrajeCclMep));
 
     tbody.appendChild(clonedRow);
 }
 
-function WriteCell(row, cellId, data) {
+function writeCell(row, cellId, data) {
     const cell = row.getElementById(cellId);
     cell.innerHTML = data;
 }
 
-function CreateTableHeader(table, titulos) {
+function createTableHeader(table, titulos) {
     const thead = table.tHead;
     const header = document.getElementById("titulos-table-header-template").content;
 
     for (let i = 0; i < titulos.length; i++) {
         thead.appendChild(header.cloneNode(true));
     }
+}
+
+function clearTableRows(table) {
+    const rows = table.rows;
+
+    for (let i = 0; i < rows.length; i++)
+        table.deleteRow(-1);
 }
 
