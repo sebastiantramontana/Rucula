48214fd4175d212f12eb34ed2d535df2f186a27b
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2023-10-09 16:27:25 -0300

    Improve publishing

---
+++ Publish/publish.cmd
@@ -0,0 +1,5 @@
+mkdir ../Release
+rem tailwind.cmd
+xcopy "../Rucula.Frontend/images" "../Release/images"
+xcopy "../Rucula.Frontend/modules" "../Release/modules"
+xcopy "../Rucula.Frontend/*.*" "../Release/*.*"
