afcc0e825868afb0b83db65689a64d0221bce880
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2023-10-31 14:44:42 -0300

    Fix titulos publicos css for caption: tailwind preprocessor doesn't recognize "#" id token

--- Publish/tailwind.cmd
+++ Publish/tailwind.cmd
@@ -1,2 +1,4 @@
-npx tailwindcss -i ../Rucula.FrontEnd/css/styles.css -o ../Release/css/styles.css --minify
+copy ..\Rucula.FrontEnd\css\titulos-publicos.css ..\Release\css\titulos-publicos.css /y
+start npx tailwindcss -i ../Rucula.FrontEnd/css/styles.css -o ../Release/css/styles.css --minify
+
 
