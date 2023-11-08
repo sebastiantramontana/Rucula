f02fc7995b03925c6692fa78358fe3f4a7f5c44f
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2023-11-08 17:53:21 -0300

    Added some global JS functions in functions.js

---
+++ WasmViewUpdater/functions.js
@@ -0,0 +1,26 @@
+﻿const viewUpdaterUpdates = {};
+
+const viewUpdaterCreateUpdateFunction = viewUpdaterCreateUpdateFunction || ((funcName, code) => {
+    const func = new Function("entity", code);
+    viewUpdaterUpdates[funcName] = func;
+});
+
+const viewUpdaterExecuteUpdateFunction = viewUpdaterExecuteUpdateFunction || ((funcName, entity) => {
+    const func = viewUpdaterUpdates[funcName];
+    func(entity);
+});
+
+const viewUpdaterGetElementById = viewUpdaterGetElementById || ((parent, id) => [parent.getElementById(id)]);
+
+const viewUpdaterGetAllElementsByQuerySelector
+    = viewUpdaterGetAllElementsByQuerySelector || ((parent, querySelector) => parent.querySelectorAll(querySelector));
+
+const viewUpdaterSetElementsContent = viewUpdaterSetElementsContent || ((elements, content) => {
+    for (const element in elements)
+        element.innerHTML = content;
+});
+
+const viewUpdaterSetElementsAttribute = viewUpdaterSetElementsAttribute || ((elements, attribute, value) => {
+    for (const element in elements)
+        element.setAttribute(attribute, value);
+});
