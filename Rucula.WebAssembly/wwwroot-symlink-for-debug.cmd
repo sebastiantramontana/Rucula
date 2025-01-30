abf3a79f2c0f07509fc29fdade344ec81a08202c
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2025-01-30 16:46:42 -0300

    Debugging: Get Rucula.Webassembly ready to be debugged. - Remove PropertyGroup for Debug condition in csproj: It crashes the debugger - Add wwwroot-symlink-for-debug.cmd to create the symlink wwwroot into Rucula.WebAssembly

---
+++ Rucula.WebAssembly/wwwroot-symlink-for-debug.cmd
@@ -0,0 +1,7 @@
+﻿@ECHO OFF
+REM El parametro %1 es la ruta raiz de Rucula, donde se encuentra la solucion de VS, y no lleva El ultimo backslash
+REM El parametro %2 es la ruta del front-end, donde se encuentra el index.html
+SET wwwrootSymLinkPath = %1\Rucula.WebAssembly\wwwroot
+SET frontEndPath = %2
+
+mklink /D %wwwrootSymLinkPath% %frontEndPath%
