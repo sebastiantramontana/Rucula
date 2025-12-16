325116ffd888b0c10f04c9ff822347e06857ec76
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2025-12-16 16:50:21 -0300

    - Refactor and redesign "Contado con Crypto" HTML, JS and CSS: Get ready for Vitraux. - Add Rucula.Presentation layer to handle all the MVP pattern: Presenters, ViewModels, action argument parsers, action binders, etc. to comunicate with view using Vitraux. - WinningChoice Presenter and ViewModel: Needs to move to ChoiceInfo... - Minor fixes:     - Disable Hot Reload in Webassembly.Mock: Bug from Blazor     - Remove unuseful System.Text.Json package.

---
+++ Rucula.Presentation/Presenters/WinningChoicePresenter.cs
@@ -0,0 +1,23 @@
+﻿using Rucula.Domain.Abstractions;
+using Rucula.Domain.Entities;
+using Rucula.WebAssembly.ViewModels;
+
+namespace Rucula.Presentation.Presenters;
+
+internal interface IWinningChoicePresenter
+{
+    Task GetChoices(WinningChoiceViewModel viewmodel, BondCommissions bondCommissions, WesternUnionParameters westernUnionParameters, DolarCryptoParameters dolarCryptoParameters);
+    Task RecalculateChoices(WinningChoiceViewModel viewmodel, BondCommissions bondCommissions, WesternUnionParameters westernUnionParameters, DolarCryptoParameters dolarCryptoParameters);
+}
+
+internal class WinningChoicePresenter(IChoicesService choiceService) : IWinningChoicePresenter
+{
+    private ChoicesInfo _currentChoices = ChoicesInfo.NoChoices;
+
+    public async Task GetChoices(WinningChoiceViewModel viewmodel, BondCommissions bondCommissions, WesternUnionParameters westernUnionParameters, DolarCryptoParameters dolarCryptoParameters)
+    {
+        _currentChoices = await choiceService.GetChoices(bondCommissions, westernUnionParameters, dolarCryptoParameters);
+    }
+
+    public Task RecalculateChoices(WinningChoiceViewModel viewmodel, BondCommissions bondCommissions, WesternUnionParameters westernUnionParameters, DolarCryptoParameters dolarCryptoParameters) => throw new NotImplementedException();
+}
---
+++ Rucula.Presentation/ViewModels/WinningChoiceViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Rucula.Domain.Entities;
+
+namespace Rucula.WebAssembly.ViewModels;
+
+public sealed class WinningChoiceViewModel
+{
+    public string Name { get; private set; } = string.Empty;
+    public string Info { get; private set; } = string.Empty;
+    public double? DolarPrice { get; private set; }
+
+    public void Update(WinningChoice winningChoice)
+    {
+        Name = winningChoice.Name;
+        Info = winningChoice.Info;
+        DolarPrice = winningChoice.DolarPrice;
+    }
+
+    public async Task GetChoices(BondCommissions bondCommissions, WesternUnionParameters westernUnionParameters, DolarCryptoParameters dolarCryptoParameters )
+    {
+
+    }
+
+    public async Task RecalculateChoices(BondCommissions bondCommissions, WesternUnionParameters westernUnionParameters, DolarCryptoParameters dolarCryptoParameters)
+    {
+
+    }
+}
