59e546686158c81fa0fe0290753e4bcc3b1a4aa7
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2026-06-12 11:07:22 -0300

    Workaround #1 for Titulos Publicos on Android: Replace WebView's User Agent from Capacitor

--- android/app/src/main/java/ar/rucula/app/MainActivity.java
+++ android/app/src/main/java/ar/rucula/app/MainActivity.java
@@ -1,5 +1,25 @@
 package ar.rucula.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+
+        String ua = settings.getUserAgentString();
+
+        ua = ua.replace("; wv", "")
+               .replace(" Version/4.0", "");
+
+        settings.setUserAgentString(ua);
+    }
+}
